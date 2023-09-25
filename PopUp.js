515fa46b2420b6c86d81fbdebd4c2ad38293fd3d
juttukondasriram <103946839+juttukondasriram@users.noreply.github.com>
2023-09-25 15:42:04 +0530

    Create PopUp.js

---
+++ PopUp.js
@@ -0,0 +1,39 @@
+import Popup from 'reactjs-popup'
+
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+const ReactPopUp = () => (
+  <div className="popup-container">
+    <Popup
+      modal
+      trigger={
+        <button type="button" className="trigger-button">
+          Rules
+        </button>
+      }
+    >
+      {close => (
+        <>
+          <div>
+            <img
+              className="popup-image"
+              src="https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rules-image.png"
+              alt="rules"
+            />
+          </div>
+          <button
+            type="button"
+            className="trigger-button"
+            onClick={() => close()}
+          >
+            Close
+          </button>
+        </>
+      )}
+    </Popup>
+  </div>
+)
+
+export default ReactPopUp
